301c82201bb08003d928c24ea8412a70129dc57e
xuguanjie <xuguanjie@halove.com>
2020-06-12 10:35:25 +0800

    ant mobile 测试

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from './index.less';
+import { Button } from 'antd-mobile';
 
 export default () => {
   return (
     <div>
       <h1 className={styles.title}>Page index</h1>
+      <Button>12313</Button>
       <div className={styles.test}>测试</div>
     </div>
   );
